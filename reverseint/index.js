169451907678ab761d4a6bf47afbc58fcc14da3b
Virtue <DOLORES A. MARGAJA@DOLORES-MARGAJA.LAPTOP-TQESST6K>
2020-06-20 15:06:20 +0800

    learning how to add, commit, and push

---
+++ reverseint/index.js
@@ -0,0 +1,124 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+// function reverseInt(n) {
+
+// 	var x = n.toString().split('').reverse().join('')
+
+// 	if (n < 0) {
+// 		return parseInt(x) * -1;
+// 	} else
+
+// 	return parseInt(x);
+// }
+
+// module.exports = reverseInt;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function reverseInt(n) {
+
+// 	var x = n.toString().split('').reverse().join('');
+// 	var y = parseInt(x) * Math.sign(n);
+// 	return y;
+// }
+
+// module.exports = reverseInt;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function reverseint(n) {
+
+// 	var x = n.toString().split('').reverse().join('');
+// 	var y = parseInt(x)
+
+// 	if (n < 0) {
+// 		return y * -1
+// 	} else return y
+// }
+
+// module.exports = reverseint;
+
